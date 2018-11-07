3b5514d294fc68dd2f8304b6b1930091bb8213a8
hugo valois <hvgaming@yahoo.com>
2018-11-07 22:08:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    
      student = self.new
     student.id, student.name, student.grade = row[0], row[1], row[2]
     student
@@ -17,7 +17,49 @@ class Student
       self.new_from_db(row)
   end
 
-  
+  def self.find_by_name(name)
+    # find the student in the database given a name	    sql = <<-SQL
+    # return a new instance of the Student class	    SELECT * 
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+   def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE students.grade < 12
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end	  end
+  	  
+def self.count_all_students_in_grade_9
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE students.grade = 9
+  SQL
+  DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
+  end
+end
   
   def save
     sql = <<-SQL
