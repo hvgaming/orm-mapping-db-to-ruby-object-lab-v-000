43f1f5cc649c872cc2a093f8be2cbbd3f4a39199
hugo valois <hvgaming@yahoo.com>
2018-11-07 22:11:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ class Student
   def self.new_from_db(row)
     
      student = self.new
-    student.id =
-     student.name =
-     student.grade = row[0]
+    student.id =row[0]
+     student.name =row[1]
+     student.grade = row[2]
     student
   end
 
